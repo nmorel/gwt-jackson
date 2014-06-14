bac62e6ec63f040b2487effb3b04ed13f0ab23b3
Nicolas Morel <nmr.morel@gmail.com>
2014-06-14 19:13:40 +0200

    Added an example using GWTP REST Dispatch
    Very basic example but at least, I'll detect if I break anything

---
+++ examples/gwtp/src/main/java/com/github/nmorel/gwtjackson/hello/shared/GreetingResponse.java
@@ -0,0 +1,34 @@
+package com.github.nmorel.gwtjackson.hello.shared;
+
+public class GreetingResponse {
+
+    private String greeting;
+
+    private String serverInfo;
+
+    private String userAgent;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting( String greeting ) {
+        this.greeting = greeting;
+    }
+
+    public String getServerInfo() {
+        return serverInfo;
+    }
+
+    public void setServerInfo( String serverInfo ) {
+        this.serverInfo = serverInfo;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent( String userAgent ) {
+        this.userAgent = userAgent;
+    }
+}
